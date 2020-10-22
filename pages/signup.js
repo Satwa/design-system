4b4f7c1e30679c091553a7bb6d0c3112a294b703
Joshua <satwa.fr@gmail.com>
2020-10-22 21:26:04 +0200

    redirect on success

--- pages/signup.js
+++ pages/signup.js
@@ -2,6 +2,7 @@ import Button from '../components/button'
 import InputBox from '../components/inputBox'
 import styles from '../styles/Home.module.scss'
 import React from 'react'
+import Router from 'next/router'
 
 export default function SignUp() {
 	const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
@@ -32,6 +33,10 @@ export default function SignUp() {
 		if(fields[2].value !== fields[3].value) {
 			setPwdErrorMessage('Les mots de passe ne correspondent pas')
 			setPwdError(true)
+		}
+
+		if(!pwdError && !emailError && !textError) {
+			Router.push('/')
 		}
 	}
 
