14e47e6e15db99a244ca39740a54da7e961b0ed8
Joshua <satwa.fr@gmail.com>
2020-10-22 21:16:43 +0200

    page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.scss'
+import { PageTransition } from 'next-page-transitions'
+import { Container } from 'next/app'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Container>
+			<PageTransition timeout={300} classNames="page-transition">
+				<Component {...pageProps} />
+			</PageTransition>
+		</Container>
+	)
 }
 
 export default MyApp
